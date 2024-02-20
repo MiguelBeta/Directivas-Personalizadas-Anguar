b897cadadb5bd42168e238d792e88944d58a7b68
Miguel <workingbeta01@gmail.com>
2024-02-20 12:17:57 -0500

    Mutaciones y señales

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter-page',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './counter-page.component.css'
 })
 export class CounterPageComponent {
+
+  public counter = signal(10);
+  //Una señal de solo lectura: se debe computar
+  public squaredCounter = computed( () => this.counter() * this.counter() );
+
+  increaseBy( value: number){
+    this.counter.update( current => current + value );
+  }
 
 }
--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
+import { UsersServiceService } from '../../services/user-service.service';
+import { User } from '../../interfaces/user-request';
+import { Console } from 'console';
 
 @Component({
   selector: 'app-user-info-page',
   templateUrl: './user-info-page.component.html',
   styleUrl: './user-info-page.component.css'
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
+
+  //Para llamar al usuario desde el servicio inyectado la info se hace asi o por el constructor,
+  //cualquiera de las dos formas es valida
+  private usersService = inject(UsersServiceService);
+  public userId = signal(1);
+
+  //Los posibles valores que pueda tener la señal (valores interfaz o undefined en caso tal de que no exista)
+  public currentUser = signal<User | undefined>(undefined);
+  public userWasFound = signal(true);
+  public fullName = computed<string>( () => {
+    if( !this.currentUser )return 'Usuario no encontrado';
+
+    return `${ this.currentUser()?.first_name } ${this.currentUser()?.last_name}`;
+  });
+
+
+
+  //Cuando el componente se monta se hace la peticion http cuando este listo el componente
+  ngOnInit(): void {
+    this.loadUser( this.userId() )
+  }
+
+  loadUser( id: number ){
+    if( id <= 0 )return;
+
+    this.userId.set(id);
+    this.currentUser.set(undefined);
+
+    //Se hace la peticion http para obtener la informacion del backend
+    this.usersService.getUserById( id )
+      .subscribe({
+        next: (user) => {
+          this.currentUser.set( user );
+          this.userWasFound.set(true);
+        },
+        error: () => {
+          this.userWasFound.set(false);
+          this.currentUser.set(undefined);
+        }
+      })
+
+  }
 
 }
